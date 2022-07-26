7bae46393d86e8ce45955809d3aabbfd552fb026
juliaperdona <juliaperdona@gmail.com>
2022-07-25 22:25:00 -0300

    commit

--- exercicios-semana-04/ex 01 - 07/index.js
+++ exercicios-semana-04/ex 01 - 07/index.js
@@ -3,16 +3,19 @@ const contasClientes = [
         id: 1,
         nome: 'Marcelo Fernandes',
         saldo: 500,
+        password: "",
     },
     {
         id: 2,
         nome: 'Carla Marcelino Cardoso',
         saldo: 3000,
+        password: "",
     },
     {
         id: 3,
         nome: 'Bruno Pedro Gonçalves',
         saldo: 5000,
+        password: "",
     },
 ];
 
@@ -21,19 +24,22 @@ const calculaValor = function () {
     let client = document.getElementById("exibirContas")
     let valor = document.getElementById("valor").value || 0
     let BankAccount = document.getElementById("saqueDeposito").value
+    let password = document.getElementById("password").value
     let newValue = Number(valor)
     contasClientes.forEach(i => {
         let option = new Option(i.nome, i.id)
         client.options[client.options.length] = option
 
-        new Set(client)
+
 
         if (client.value == i.id && BankAccount == "sacar") {
+            item.password = password
             return console.log(i.saldo - newValue)
         }
         if (client.value == i.id && BankAccount == "depositar") {
+            item.password = password
             return console.log(i.saldo + newValue)
         }
-
+        new Set(client)
     })
 }
